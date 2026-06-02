06f6fe627e66fa4730b515deffc19843833baffc
Morteza Khojasteh <mori.kh1987@gmail.com>
2026-06-02 11:54:16 +0100

    Added Handler for server unavailable

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 let mainWindow;
 
@@ -7,11 +8,19 @@ function createWindow() {
     width: 1280,
     height: 900,
 
+    // Enable kiosk mode
+    kiosk: true,
+
     webPreferences: {
       preload: `${__dirname}/preload.js`,
       contextIsolation: true,
       nodeIntegration: false,
     },
+  });
+
+  // Handle server unavailable
+  mainWindow.webContents.on("did-fail-load", () => {
+    mainWindow.loadFile(path.join(__dirname, "offline.html"));
   });
 
   mainWindow.loadURL("http://localhost:5173/clockapp");
