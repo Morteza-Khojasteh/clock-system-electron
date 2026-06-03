029d3a7044c5b86608664bc8c58ce42c620f6df5
Morteza Khojasteh <mori.kh1987@gmail.com>
2026-06-03 08:46:49 +0100

    updated createCloseWindow file

--- app/windows/createSetupWindow.js
+++ app/windows/createSetupWindow.js
@@ -4,13 +4,15 @@ const path = require("path");
 function createSetupWindow() {
   const win = new BrowserWindow({
     width: 700,
-    height: 500,
+    height: 540,
     resizable: false,
     autoHideMenuBar: true,
+    icon: path.join(__dirname, "../assets/icon.ico"),
     webPreferences: {
       preload: path.join(__dirname, "../preload/setup-preload.js"),
       contextIsolation: true,
       nodeIntegration: false,
+      sandbox: true,
     },
   });
 
